f1d7b821998581ecab283321c07bbe33732ac8e9
Alex <83047121+GiedraAlexandra19@users.noreply.github.com>
2022-09-21 21:29:10 -0500

    actualizado

--- backend/models/ponente.py
+++ backend/models/ponente.py
@@ -1,5 +1,5 @@
 from backend.models.usuario import UsuariosModel
 
-class PonenteModel(UsuariosModel):
+class ponenteModel(UsuariosModel):
     def __init__(self, id, nombre, apellido, correo):
         UsuariosModel.__init__(self, id, nombre, apellido, correo)
