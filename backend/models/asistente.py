344b75d77ecb4a0d92a1c2f537b0d49f35cfaf91
Alex <83047121+GiedraAlexandra19@users.noreply.github.com>
2022-09-21 21:28:28 -0500

    actualizado

--- backend/models/asistente.py
+++ backend/models/asistente.py
@@ -1,5 +1,5 @@
 from backend.models.usuario import UsuarioModel
 
-class AsistenteModel(UsuarioModel):
+class asistenteModel(UsuarioModel):
     def __init__(self, id_, nombre_, apellido_, correo_):
         UsuarioModel.__init__(self, id_, nombre_, apellido_, correo_)
